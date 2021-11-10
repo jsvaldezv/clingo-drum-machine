90f0e57abc6279eafb4acff34ea7fd257e5a2167
jsvaldezv <jsvaldezv@gmail.com>
2021-11-09 21:00:53 -0600

    Add Num Compases

--- Python/main.py
+++ Python/main.py
@@ -10,69 +10,32 @@ class Main(QMainWindow, QWidget):
         super().__init__()
         self.resize(480, 300)
 
+        self.path = "none"
+
         # LOAD AUDIOS #
         self.btnMix = QPushButton('Create', self)
         self.btnMix.setGeometry(10, 10, 200, 50)
-        self.btnMix.clicked.connect(lambda: self.plotAudio())
+        self.btnMix.clicked.connect(lambda: self.startCreating())
 
         self.boxAudio = dragAudio.ListboxWidget(self)
         self.boxAudio.setPos(250, 15)
 
-        self.makeKickPattern()
-
     def plotAudio(self):
-        path = QListWidgetItem(self.boxAudio.item(0))
-        if path.text():
-            path = path.text()
-            audio, samplerate = sf.read(path)
+        self.path = QListWidgetItem(self.boxAudio.item(0))
+        if self.path.text():
+            self.path = self.path.text()
+            audio, samplerate = sf.read(self.path)
             self.chart = utilitiesGUI.Canvas(self)
             self.chart.plotAudio(audio)
             self.chart.setGeometry(10, 80, 460, 200)
             self.chart.show()
+        print("AUDIO PLOTTED")
+        print("-------------")
 
-    def makeKickPattern(self):
-        audio, samplerate = sf.read('../audios/kick.wav')
-        kickInterval = utilitiesGUI.computeInterval(120, samplerate)
-        arraY = []
-        cont = 0
+    def startCreating(self):
+        self.plotAudio()
+        utilitiesGUI.makeKickPattern(self.path, 120, 4)
 
-        print(kickInterval)
-        sample1 = kickInterval
-        sample2 = kickInterval * 2
-        sample3 = kickInterval * 3
-        sample4 = kickInterval * 4
-
-        cont = 0
-        cont2 = 0
-        cont3 = 0
-        cont4 = 0
-        for sample in range(kickInterval * 4):
-            if sample < sample1:
-                if cont < len(audio):
-                    arraY.append(audio[sample])
-                else:
-                    arraY.append(0)
-                cont += 1
-            elif sample >= sample1 and sample < sample2:
-                if cont2 < len(audio):
-                    arraY.append(audio[cont2])
-                else:
-                    arraY.append(0)
-                cont2 += 1
-            elif sample >= sample2 and sample < sample3:
-                if cont3 < len(audio):
-                    arraY.append(audio[cont3])
-                else:
-                    arraY.append(0)
-                cont3 += 1
-            else:
-                if cont4 < len(audio):
-                    arraY.append(audio[cont4])
-                else:
-                    arraY.append(0)
-                cont4 += 1
-
-        sf.write('../Results/Attack.wav', arraY, samplerate, 'PCM_24')
 
 '''def applyEnvelope(inAudio, inSampleRate, inAttack, inRelease):
 
--- Python/utilitiesGUI.py
+++ Python/utilitiesGUI.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import soundfile as sf
 
 class Canvas(FigureCanvas):
 
@@ -15,3 +16,51 @@ class Canvas(FigureCanvas):
 def computeInterval(inBpm, inSampleRate):
     interval = int((60 / inBpm) * inSampleRate)
     return interval
+
+def makeKickPattern(inPath, inBPM, inNumCompas):
+
+    audio, samplerate = sf.read(inPath)
+    kickInterval = computeInterval(inBPM, samplerate)
+    loopKick = []
+
+    intervalOne = kickInterval
+    intervalTwo = kickInterval * 2
+    intervalTres = kickInterval * 3
+
+    cont, cont2, cont3, cont4 = 0, 0, 0, 0
+
+    for sample in range(kickInterval * 4):
+        if sample < intervalOne:
+            if cont < len(audio):
+                loopKick.append(audio[sample])
+            else:
+                loopKick.append(0)
+            cont += 1
+        elif sample >= intervalOne and sample < intervalTwo:
+            if cont2 < len(audio):
+                loopKick.append(audio[cont2])
+            else:
+                loopKick.append(0)
+            cont2 += 1
+        elif sample >= intervalTwo and sample < intervalTres:
+            if cont3 < len(audio):
+                loopKick.append(audio[cont3])
+            else:
+                loopKick.append(0)
+            cont3 += 1
+        else:
+            if cont4 < len(audio):
+                loopKick.append(audio[cont4])
+            else:
+                loopKick.append(0)
+            cont4 += 1
+
+    compasCompleto = []
+    for compas in range(inNumCompas):
+        for sample in loopKick:
+            compasCompleto.append(sample)
+
+    sf.write('../Results/LoopKick.wav', compasCompleto, samplerate, 'PCM_24')
+
+    print("KICK LOOP CREADO")
+    print("-----------")
